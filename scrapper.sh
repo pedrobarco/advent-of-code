2781fce5fb4eb825b4a93fa338c5bb53d5282175
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2023-12-20 20:15:51 +0000

    refactor: move scrapper to root folder

--- scrapper.sh
+++ scrapper.sh
@@ -17,15 +17,18 @@ then
 fi
 
 url="https://adventofcode.com/$year/day/{DAY}/input"
-input="./input/day{DAY}.in"
+input_dir="./$year/input"
+input_tmpl="$dir/day{DAY}.in"
+
+mkdir -p "$input_dir"
 
 for i in {1..25}
 do
     day=$(printf "%02d" $i)
     input_url=${url/\{DAY\}/$i}
-    input_file=${input/\{DAY\}/$day}
+    input_file=${input_tmpl/\{DAY\}/$day}
     echo "Downloading day $i input..."
-    curl --cookie "session=$AOC_TOKEN" "$input_url" -o "$input_file"
+    curl --cookie "session=$AOC_TOKEN" -sSfo "$input_file" "$input_url"
     echo "saved: $input_file"
     echo
 done
